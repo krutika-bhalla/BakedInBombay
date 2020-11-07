175a842c39cbb921621f0b350db45d3db66148ea
Krutika Bhalla <krutika.bhalla@somaiya.edu>
2020-11-07 23:04:42 +0530

    :rocket:

--- bib/resources/views/layouts/app.blade.php
+++ bib/resources/views/layouts/app.blade.php
@@ -97,7 +97,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                                    {{ ucwords(Auth::user()->name) }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
@@ -117,6 +117,7 @@
                 </div>
             </div>
         </nav>
+        </header>
         </header>
 
         <main class="py-4">
